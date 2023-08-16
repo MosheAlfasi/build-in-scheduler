c1590eab4ad86d238c4e09ff9dbc89610e5cc805
mosheal <mosheal@radware.com>
2023-08-16 21:06:35 +0300

    Initial commit

---
+++ src/main/java/com/lym/buildin/scheduler/BuildInSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.lym.buildin.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildInSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildInSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lym/buildin/scheduler/BuildInSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lym.buildin.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildInSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
